2637765c0bd99c05d3ca4d18c09e1267231bd250
Eugene Slutsky <eslutckii@twitter.com>
2025-08-29 17:24:49 -0400

    indexing all code by feature flag

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/workspace/BazelProjectDirectoriesWorkspaceFileIndexContributor.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/workspace/BazelProjectDirectoriesWorkspaceFileIndexContributor.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.bazel.workspace
 
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.platform.workspace.storage.EntityStorage
+import com.intellij.platform.workspace.storage.url.VirtualFileUrl
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndexContributor
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileKind
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileSetRegistrar
@@ -36,15 +38,47 @@ class BazelProjectDirectoriesWorkspaceFileIndexContributor : WorkspaceFileIndexC
     )
   }
 
-  private fun WorkspaceFileSetRegistrar.registerIncludedDirectories(entity: BazelProjectDirectoriesEntity) =
-    entity.includedRoots.forEach {
+  private fun WorkspaceFileSetRegistrar.registerIncludedDirectories(entity: BazelProjectDirectoriesEntity) {
+    if (!Registry.`is`("bazel.x.index.all", false)) {
       registerFileSet(
-        root = it,
+        root = entity.projectRoot,
         kind = WorkspaceFileKind.CONTENT,
         entity = entity,
         customData = null,
       )
+    } else {
+      val includedGrouped = entity.includedRoots.groupBy {
+        it.url.contains("strato/config")
+      }
+      val nonStrato = includedGrouped[false] ?: emptyList()
+      val strato = includedGrouped[true] ?: emptyList()
+
+      nonStrato.forEach {
+        registerFileSet(
+          root = it,
+          kind = WorkspaceFileKind.CONTENT,
+          entity = entity,
+          customData = null,
+        )
+      }
+      if (strato.isNotEmpty()) {
+        var stratoDir: VirtualFileUrl? = strato.first()
+        while (stratoDir != null && !stratoDir.url.endsWith("/strato/config")) {
+          stratoDir = stratoDir.parent
+        }
+        if (stratoDir != null) {
+          listOf(stratoDir, stratoDir.append(".thrift_deps")).forEach { stratoDirsToIndex ->
+            registerFileSet(
+              root = stratoDirsToIndex,
+              kind = WorkspaceFileKind.CONTENT,
+              entity = entity,
+              customData = null,
+            )
+          }
+        }
+      }
     }
+  }
 
   private fun WorkspaceFileSetRegistrar.registerExcludedDirectories(entity: BazelProjectDirectoriesEntity) {
     excludeSymlinksFromFileWatcher(entity.excludedRoots.map { it.toPath() })
--- versions.bzl
+++ versions.bzl
@@ -1,11 +1,19 @@
 """Developer versions of plugins."""
 
-INTELLIJ_BAZEL_VERSION = "9999.9.9"
+<<<<<<< HEAD
+INTELLIJ_BAZEL_VERSION = "7025.2.1-001x"
+=======
+INTELLIJ_BAZEL_VERSION = "7025.2.1-001x"
+>>>>>>> f6b192c78 (indexing all code by feature flag)
 
 PLATFORM_VERSION = "252"
 
 # make sure to always choose the lowest among different benchmark build numbers
-SINCE_VERSION = "252.23892"
+<<<<<<< HEAD
+SINCE_VERSION = "252.25557.131"
+=======
+SINCE_VERSION = "252.25557.131"
+>>>>>>> f6b192c78 (indexing all code by feature flag)
 UNTIL_VERSION = "252.*"
 
 BENCHMARK_BUILD_NUMBER = "252.23892.409"
